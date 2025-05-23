b4f1e76b8ff25ee724c99bf8ec5773f9daaa1f14
Alexander <sanyatik@gmail.com>
2025-05-23 08:08:51 +0300

    add functional

--- Lab2/Lab2/MainWindow.xaml.cs
+++ Lab2/Lab2/MainWindow.xaml.cs
@@ -1,27 +1,70 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Lab2
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private string _copiedText = "";
+
         public MainWindow()
         {
             InitializeComponent();
+            fontSlider.ValueChanged += FontSlider_ValueChanged;
         }
 
-        private void fontSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        // Слайдер шрифта
+        private void FontSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (mainTextBox != null)
+                mainTextBox.FontSize = fontSlider.Value;
+        }
+
+        // Открыть файл
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFile.ShowDialog() == true)
+            {
+                mainTextBox.Text = File.ReadAllText(openFile.FileName);
+            }
+        }
+
+        // Очистить текст
+        private void ClearText_Click(object sender, RoutedEventArgs e)
+        {
+            mainTextBox.Clear();
+        }
+
+        // Копировать
+        private void CopyText_Click(object sender, RoutedEventArgs e)
+        {
+            _copiedText = mainTextBox.SelectedText;
+            Clipboard.SetText(_copiedText);
+        }
+
+        // Вставить
+        private void PasteText_Click(object sender, RoutedEventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                int caret = mainTextBox.CaretIndex;
+                mainTextBox.Text = mainTextBox.Text.Insert(caret, Clipboard.GetText());
+                mainTextBox.CaretIndex = caret + Clipboard.GetText().Length;
+            }
+        }
+
+        // OK-кнопка (например, показать сообщение)
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("OK натиснуто!", "Повідомлення");
+        }
+
+        private void mainTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
